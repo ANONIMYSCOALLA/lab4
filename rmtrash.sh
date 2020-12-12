aab7f41373ad0d51e07dddc1208bb9d931949166
ANONIMYSCOALLA <58992611+ANONIMYSCOALLA@users.noreply.github.com>
2020-12-12 13:08:21 +0300

    Update rmtrash.sh

--- rmtrash.sh
+++ rmtrash.sh
@@ -2,20 +2,20 @@
 
 if [[ ! -e "$PWD/.rmtrash.log" ]]
 then
-	echo 1 > "$PWD/.rmtrash.log"
+echo 1 > "$PWD/.rmtrash.log"
 fi
 
 if [[ $# -gt 1 ]]
 then
-	echo "Incorrect number of arguments. Had to be 1."
-	exit 1
+echo "Wrong amount of arguments. Had to be 1."
+exit 1
 fi
 
 
 if [[ ! -f $1 ]]
 then
-	echo "$1: No such file exists"
-	exit 1
+echo "$1: File don't exist"
+exit 1
 fi
 
 
@@ -23,7 +23,7 @@ dir="$HOME/.trash"
 dir_log="$HOME/.trash.log"
 if [[ ! -d $dir ]]
 then
-	mkdir "$dir"
+mkdir "$dir"
 fi
 
 
@@ -35,6 +35,3 @@ touch "$tmp_ln"
 (( numberOfLinks=numberOfLinks+1 ))
 echo "$numberOfLinks" > "$PWD/.rmtrash.log"
 rm "$tmp"
-
-
-echo "$tmp $tmp_ln" >> "$dir_log"
